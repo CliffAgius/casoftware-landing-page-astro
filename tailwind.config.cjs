4d82f3e4fb95269f12b5c46a12e92f54613a1ce7
David Smith <96678397+Moomins07@users.noreply.github.com>
2023-06-07 18:39:03 +0100

    'Updated to CASoftware landing page'

---
+++ tailwind.config.cjs
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1020px',
+      xl: '1440px',
+    },
+    extend: {
+      backgroundImage: (theme) => ({
+        wallpaper: "url('../src/images/gradient-blue.jpg')",
+      }),
+      zIndex: {
+        10: '10',
+      },
+      colors: {
+        softBlue: 'hsl(231, 69%, 60%)',
+        softRed: 'hsl(0, 94%, 66%)',
+        grayishBlue: 'hsl(229, 8%, 60%)',
+        veryDarkBlue: 'hsl(229, 31%, 21%)',
+      },
+      fontFamily: {
+        sans: ['Rubik', 'sans-serif'],
+      },
+      scale: ['group-hover'],
+      textColor: ['group-hover'],
+      backgroundColor: ['group-hover'],
+      opacity: ['group-hover'],
+    },
+  },
+  plugins: [],
+};
