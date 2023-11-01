f77e075a5cb970ec45a5f883e1ccade3aa543ba5
Clifford Agius <CliffordAgius@ca-software.co.uk>
2023-11-01 15:55:00 +0000

    Fixed outdated config settings from blog used to set-up this build pipeline...

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-export default {
-  buildOptions: {
-    site: 'https://kind-bush-0d0d21403.4.azurestaticapps.net',
-  },
-  defineConfig: {
-    integrations: [tailwind()],
-  }
-};
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  site: 'https://kind-bush-0d0d21403.4.azurestaticapps.net',
+});
