f1b06bda11954bbc7048d4e448a783e51a104dfe
Clifford Agius <CliffordAgius@ca-software.co.uk>
2023-11-01 15:50:28 +0000

    Updating the config so that it builds in Azure...

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()],
-});
+export default {
+  buildOptions: {
+    site: 'https://kind-bush-0d0d21403.4.azurestaticapps.net',
+  },
+  defineConfig: {
+    integrations: [tailwind()],
+  }
+};
